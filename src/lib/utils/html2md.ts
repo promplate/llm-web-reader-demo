9512fcbfbc61a99d76e12345e6a0339e035de0fe
Muspi Merol <wjj123345567@icloud.com>
2024-10-13 06:16:09 +0800

    feat: support gfm

--- src/lib/utils/html2md.ts
+++ src/lib/utils/html2md.ts
@@ -1,5 +1,7 @@
 import TurndownService from "turndown"
+// @ts-expect-error Missing types
+import { gfm } from "turndown-plugin-gfm"
 
-const turndownService = new TurndownService({ headingStyle: "atx", codeBlockStyle: "fenced", bulletListMarker: "-" })
+const turndownService = new TurndownService({ headingStyle: "atx", codeBlockStyle: "fenced", bulletListMarker: "-" }).use(gfm)
 
 export const toMarkdown = turndownService.turndown.bind(turndownService)
